d7d89911064ba7d5a8b6062d3dd7c44cd7e74500
Adrian Li <li.adrianmc@gmail.com>
2016-12-22 21:52:09 -0500

    update server code to use react-router for server side rendering of react

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 import express from 'express';
 import http from 'http';
 
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+
+import AppComponent from './components/app';
+import IndexComponent from './components/index';
+
+const routes = {
+  path: '',
+  component: AppComponent,
+  childRoutes: [
+    {
+      path: '/',
+      component: IndexComponent,
+    },
+  ],
+};
+
 const app = express();
 
 app.use(express.static('public'));
@@ -8,7 +26,28 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('*', (req, res) => {
-  res.render('index');
+  // matches a set of routes to a location, without rendering,
+  // and calls a callback when it's done.
+  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+    // 4 possibilities:
+    // 1. an error occured, respond with 500
+    // 2. ReactRouter redirect, so trigger a redirect from the server
+    // 3. Valid component found for the given route, use renderProps
+    //    to render the app and convert it to a string to be passed
+    //    into the EJS template
+    // 4. Route not found, respond with 404
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (renderProps) {
+      const markup = renderToString(<RouterContext {...renderProps} />);
+      console.log(markup);
+      res.render('index', { markup });
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
 
 const server = http.createServer(app);
